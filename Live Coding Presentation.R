aa76220b71431e624cd572cf8938e7a78d4481d3
Phillip Lear <philliplear@gmail.com>
2022-02-16 17:43:45 -0500

    Changed for large font display

--- Live Coding Presentation.R
+++ Live Coding Presentation.R
@@ -6,10 +6,13 @@ library(tidyverse)
 
 
 # URL location of data
-url_counties <- "https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv"
-url_college <- "https://raw.githubusercontent.com/nytimes/covid-19-data/master/colleges/colleges.csv"
+url_counties <- 
+  "https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv"
+url_college <- 
+  "https://raw.githubusercontent.com/nytimes/covid-19-data/master/colleges/colleges.csv"
 
-covid_data <- read_csv(url_counties)  # This is over 2 million records so it may take a while to download
+covid_data <- read_csv(url_counties)  
+# This is over 2 million records so it may take a while to download
 covid_dataCollege <- read_csv(url_college)
 
 
